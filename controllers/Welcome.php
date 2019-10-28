4e72ceb138624d9bf29a39cfdbb73bf1acfcfd1b
khushmohammad <49813728+khushmohammad@users.noreply.github.com>
2019-10-28 23:49:46 +0400

    welcome

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -5,7 +5,7 @@ class Welcome extends CI_Controller {
 	function __construct()  
 		{  
 			parent::__construct();    
-			$this->load->model('LoginModel');  
+			$this->load->model('LoginModel');    
 		} 
 	
 
